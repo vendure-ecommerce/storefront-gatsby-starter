23127b845e38dc4bf744f1a06ea6cf42a68d8e01
Michael Bromley <michael@michaelbromley.co.uk>
2022-01-23 18:08:20 +0000

    Initial commit - Gatsby + Tailwind

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,7 +8,7 @@ import Seo from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <Seo title="Home" />
-    <h1>Hi people</h1>
+    <h1 className='text-3xl'>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
     <StaticImage
